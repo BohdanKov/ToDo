2bdc78bcbd9f14e16fac61fb10071fc3586dfc16
BohdanKov <bohdankov@tahometer.com>
2020-08-29 19:33:15 +0300

    Only one list 'toDo' with GET and POST

---
+++ Controllers/TaskController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using TodoList.Models;
+using TodoList.Controllers;
+using Microsoft.Data.Sqlite;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TodoList.Models
+{
+    [Route("api/tasks")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        public static DatabaseService dataBaseS;
+
+        static TaskController()
+        {
+            dataBaseS = new DatabaseService();
+        }
+        // GET: api/tasks
+        [HttpGet("")]
+        public IActionResult Get()
+        {
+            return Ok(dataBaseS.Select());
+        }
+
+        // POST api/tasks
+        [HttpPost("")]
+        public IActionResult Post(Task task)
+        {
+            return Ok(dataBaseS.Create(task));
+        }
+    }
+}
---
+++ Services/DatabaseService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+using TodoList.Models;
+using TodoList.Controllers;
+
+
+
+namespace TodoList.Models
+{
+    public class DatabaseService
+    {
+        public DatabaseService()
+        {
+            SetUpDatabase();
+        }
+
+        public void SetUpDatabase()
+        {
+            using (var connection = ProvideConnection())
+            {
+                BuildSqlCommand(connection, "CREATE TABLE IF NOT EXISTS toDo(id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(50), done TINYINT)").ExecuteNonQuery();
+            }
+        }
+
+        public SqliteConnection  ProvideConnection()
+        {
+            var conBuilder = new SqliteConnectionStringBuilder { DataSource = "./todos.db" };
+            return new SqliteConnection(conBuilder.ConnectionString);
+        }
+
+        public SqliteCommand BuildSqlCommand(SqliteConnection connection, string query)
+        {
+            var cmd = connection.CreateCommand();
+            cmd.Connection.Open();
+            cmd.CommandText = query;
+            return cmd;
+        }
+
+        internal Task Create(Task task)
+        {
+            using (var connection = ProvideConnection())
+            {
+                BuildSqlCommand(connection, $"INSERT INTO toDo(name, done) VALUES('{task.name}','{task.done}')").ExecuteNonQuery();
+            }
+            return task;
+        }
+
+        public List<Task> Select()
+        {
+            var listOfTasks = new List<Task>();
+            using (var connection = ProvideConnection())
+            {
+                using (var reader = BuildSqlCommand(connection, "SELECT * FROM toDo").ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        listOfTasks.Add(new Task(reader.GetInt32(0), reader.GetString(1), reader.GetBoolean(2)));
+                    }
+                }
+            }
+            return listOfTasks;
+        }
+
+    }
+}
