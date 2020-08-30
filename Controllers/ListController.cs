0ad677c9c5e1b0237c74a1718630d8dfdfe96dfd
BohdanKov <bohdankov@tahometer.com>
2020-08-31 01:27:50 +0300

    Server can work with lists too

---
+++ Controllers/ListController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TodoList.Models
+{
+    [Route("api/lists")]
+    [ApiController]
+    public class ListController : ControllerBase
+    {
+        [HttpGet("")]
+        public IActionResult Get()
+        {
+            return Ok(ListDataBaseService.ShowAllLists());
+        }
+
+        [HttpPost("{listname}")]
+        public void Post(string listname)
+        { 
+            ListDataBaseService.CreateNewList(listname);
+        }
+
+        [HttpPatch("{listname}/{newname}")]
+        public void Patch(string listname, string newname)
+        {
+            ListDataBaseService.RenameList(listname, newname);
+        }
+
+        [HttpDelete("{listname}")]
+        public void Delete(int id, string listname)
+        {
+            ListDataBaseService.RemoveList(listname);
+        }
+    }
+}
---
+++ Services/ListDataBaseService.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoList.Models
+{
+    public class ListDataBaseService
+    {
+        public ListDataBaseService(string listname)
+        {
+            DatabaseService.SetUpDatabase(listname);
+        }
+
+        // GET
+        public static ArrayList ShowAllLists()
+        {
+            ArrayList table = new ArrayList();
+            DataTable temp = new DataTable();
+            using (var connection = DatabaseService.ProvideConnection())
+            {
+                var reader = DatabaseService.BuildSqlCommand(connection, $"SELECT name FROM sqlite_master WHERE type = 'table' != '%sqlite_sequence%'").ExecuteReader();
+                temp.Load(reader);
+
+            }
+            foreach (DataRow row in temp.Rows)
+            {
+                table.Add(row.ItemArray[0].ToString());
+            }
+            return table;
+
+        }
+
+        // POST
+        public static void CreateNewList(string listname)
+        {
+            DatabaseService.SetUpDatabase(listname);
+        }
+
+        // PATCH
+        public static void RenameList(string listname, string newname)
+        {
+            using (var connection = DatabaseService.ProvideConnection())
+            {
+                DatabaseService.BuildSqlCommand(connection, $"ALTER TABLE {listname} RENAME TO {newname}").ExecuteNonQuery();
+            }
+        }
+
+        // DELETE 
+        public static void RemoveList(string listname)
+        {
+            using (var connection = DatabaseService.ProvideConnection())
+            {
+                DatabaseService.BuildSqlCommand(connection, $"DROP TABLE IF EXISTS {listname}").ExecuteNonQuery();
+            }
+        }
+
+
+
+
+        
+    }
+}
